89c048bf666ca3863854c4fcc9a62d3d75dcf582
Aparna0327 <aparnashivasamy@gmail.com>
2025-06-19 16:22:57 +0530

    first

---
+++ java.js
@@ -0,0 +1,13 @@
+let a = 10;
+.
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+let name = kowsik.20
+
+
+-/;
+let isbool=true;
+let c;
+let b=null;
+let abc=235689;
+console.log(\
+     
